d229ff7843710a1b37029e827ac215684545faf9
Vidineev Sergey <vidiac@yandex.ru>
2019-06-26 13:07:15 +1000

    update input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,24 @@
 import {h, Component} from "preact";
 import PhoneInput from "./PhoneInput";
-import IMask from 'imask'
 
 class App extends Component {
-    ref = null
     state = {
-        phoneNumber: '',
-    }
-
-    componentDidMount()
-    {
-        let lastChar = ''
-        const phoneMask = IMask(
-            this.ref, 
-            { 
-                mask: '+{7} 000-000-00-00', 
-                prepare: function (str, masked) {
-                    const {typedValue, unmaskedValue, rawInputValue} = masked
-                    console.log({str, typedValue, unmaskedValue, rawInputValue})
-                    if (str === '7' && lastChar === '+') {
-                        return ''
-                    }
-                    lastChar = str
-                    return str
-                },
-             }
-        )
+        phoneNumber: '79243242273',
     }
 
     onChangePhoneNumber = phone => {
         this.setState({phoneNumber: phone})
     }
 
-    onInput = e => {
-        if (e.inputType === 'insertFromPaste') {
-            e.currentTarget.value = ''
-        }
-    }
-
     render(props, {phoneNumber}) {
         return (
             <div>
-                <input type="text" ref={ref => this.ref = ref}/>
-                {/* <PhoneInput
+                <PhoneInput
                     onChange={this.onChangePhoneNumber}
                     value={phoneNumber}
-                    mask='+7 ___-___-__-__'
                     placeholder='+7 ___-___-__-__'
-                /> */}
+                />
+                <div>{phoneNumber}</div>
             </div>
         )
     }
--- src/components/PhoneInput.jsx
+++ src/components/PhoneInput.jsx
@@ -1,68 +1,51 @@
-import {h, Component} from "preact";
+import {h, Component} from "preact"
+import IMask from 'imask'
 
 class PhoneInput extends Component {
+    imask = null
     ref = null
-    lastCaretPosition = {start: null, end: null}
 
-    onChange = e => {
-        const currentPosition = PhoneInput.getCaretPos(e.currentTarget)
+    componentDidMount()
+    {
+        let lastChar = ''
+
+        this.imask = IMask(
+            this.ref, 
+            { 
+                mask: '+{7} 000-000-00-00', 
+                prepare: function (str, masked) {
+                    const {typedValue, unmaskedValue, rawInputValue} = masked
+                    if (str === '7' && lastChar === '+') {
+                        return ''
+                    }
+
+                    if (str === '8' && rawInputValue === '') {
+                        return ''
+                    }
+
+                    if (str === '+' && rawInputValue === '') {
+                        return ''
+                    }
 
 
-        const {onChange} = this.props
-        const {value} = e.currentTarget
-        const clearValue = PhoneInput.clearValue(value)
-
-        // if (this.props.value.length > clearValue.length && currentPosition.end < this.lastCaretPosition.end) {
-        //     requestAnimationFrame(() => {
-        //         PhoneInput.setCaretPos(this.ref, currentPosition)
-        //     })
-        // }
-
-        onChange(clearValue)
-        this.lastCaretPosition = {...currentPosition}
-    }
-
-    static format(value, mask) {
-        let i = 0;
-        let lastReplacedIndex = -1;
-        const filledMask = mask.replace(/_/g, (_, j) => {
-            if (i >= value.length) {
-                return '_';
-            }
-
-            lastReplacedIndex = j;
-            return value[i++]
+                    lastChar = str
+                    return str
+                },
+             }
+        ),
+        this.imask.on('accept', () => {
+            this.props.onChange(this.imask.unmaskedValue)
         });
-
-        return filledMask.substring(0, lastReplacedIndex + 1);
     }
 
-    static clearValue(value) {
-        return value.replace(/[^\d]/g, '')
+    componentWillUnmount() {
+        this.imask.off('accept');
+        this.imask.destroy()
     }
 
-    static getCaretPos(input) {
-        if (input.selectionStart || input.selectionStart === '0') {
-            return {'start': input.selectionStart, 'end': input.selectionEnd}
-        } else {
-            return {'start': 0, 'end': 0}
-        }
-    }
-
-    static setCaretPos(input, {start, end}) {
-        input.focus()
-        input.setSelectionRange(start, end)
-    }
-
-    render({mask, value, placeholder}) {
+    render({value, placeholder, onChange}) {
         return (
-            <input
-                ref={ref => (this.ref = ref)}
-                type='text'
-                value={PhoneInput.format(value, mask)}
-                placeholder={placeholder}
-                onInput={this.onChange}
-            />
+            <input type="text" defaultValue={value} placeholder={placeholder} ref={ref => this.ref = ref}/>
         )
     }
 }
